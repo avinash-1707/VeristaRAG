1ca122241781cbb7c13037e0558e50818f8e0725
Avinash <avinashnarwariya5@gmail.com>
2026-05-13 16:31:59 +0530

    worker moved with django

---
+++ backend/start.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+set -e
+
+python manage.py migrate --noinput
+
+celery -A config worker --loglevel=info --concurrency=2 &
+
+exec gunicorn config.wsgi:application \
+    --bind 0.0.0.0:8000 \
+    --workers 4 \
+    --timeout 120
